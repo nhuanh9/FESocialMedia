045ad0b4d9e9e26ca8a9320c57fcef8c7e9362f6
toannvph05233 <johntoan98@gmail.com>
2021-01-12 11:18:55 +0700

    up web socket

--- src/app/websocket/websocket.component.ts
+++ src/app/websocket/websocket.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Stomp} from '@stomp/stompjs';
 import {log} from 'util';
+import {User} from '../model/user';
 
 @Component({
   selector: 'app-websocket',
@@ -10,13 +11,15 @@ import {log} from 'util';
 export class WebsocketComponent implements OnInit {
   title = 'grokonez';
   description = 'Angular-WebSocket Demo';
-
+  user: User;
   greetings: string[] = [];
   disabled = true;
   name: string;
+  message: string;
   private stompClient = null;
 
-  constructor() { }
+  constructor() {
+  }
 
   setConnected(connected: boolean) {
     this.disabled = !connected;
@@ -26,18 +29,34 @@ export class WebsocketComponent implements OnInit {
     }
   }
 
-  connect() {
+  // connect() {
+  //   const socket = new WebSocket('ws://localhost:8080/gkz-stomp-endpoint/websocket');
+  //   this.stompClient = Stomp.over(socket);
+  //   const thisSocket = this;
+  //   this.stompClient.connect({}, function (frame) {
+  //     thisSocket.setConnected(true);
+  //     console.log('Connected: -----' );
+  //     console.log('Connected: ' + frame);
+  //
+  //     thisSocket.stompClient.subscribe('/topic/hii', function (hello) {
+  //       thisSocket.showGreeting(JSON.parse(hello.body).greeting);
+  //     });
+  //   });
+  // }
+  connect(username, userNameFriend) {
+    userNameFriend = 'johntoan';
     const socket = new WebSocket('ws://localhost:8080/gkz-stomp-endpoint/websocket');
     this.stompClient = Stomp.over(socket);
     const thisSocket = this;
-    this.stompClient.connect({}, function (frame) {
+    this.stompClient.connect({}, function(frame) {
       thisSocket.setConnected(true);
-      console.log('Connected: -----' );
+      console.log('Connected: -----');
       console.log('Connected: ' + frame);
 
-      thisSocket.stompClient.subscribe('/topic/hii', function (hello) {
-        thisSocket.showGreeting(JSON.parse(hello.body).greeting);
-      });
+      thisSocket.stompClient.subscribe('/topic/public/' + username + '/' + userNameFriend);
+      thisSocket.stompClient.send('/gkz/chatVsUser', {},
+        JSON.stringify({'name': this.name, 'message': this.message, 'userNameFriend': userNameFriend}));
+
     });
   }
 
@@ -54,15 +73,19 @@ export class WebsocketComponent implements OnInit {
     console.log('AAAAAAAAA');
     console.log(this.stompClient);
     this.stompClient.send(
-      '/gkz/hello',
+      '/gkz/chat.newUser',
       {},
-      JSON.stringify({ 'name': this.name })
+      JSON.stringify({'name': this.user.username, 'message': this.message})
     );
   }
+
   showGreeting(message) {
     this.greetings.push(message);
   }
 
   ngOnInit(): void {
+    this.user = JSON.parse(localStorage.getItem('currentUser'));
+    console.log(this.user);
+    console.log(this.user.username);
   }
 }
